ecc21fa5128e65690a7bf1bad66ba41543773390
Dile <duyle131103@gmail.com>
2024-05-20 23:51:19 +0700

    Add connect MongoDB and env variables

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,1 +1,30 @@
-//
+const MONGODB_URI = 'mongodb+srv://leanhduy:VCMIytSum4izjxCp@cluster0.xbgkos5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const DATABASE_NAME = 'trello-database'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import { env } from '~/config/environment'
+
+let trelloDatabaseInstance = null
+
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect()
+
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+}
+
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
+}
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to Database first!')
+  return trelloDatabaseInstance
+}
