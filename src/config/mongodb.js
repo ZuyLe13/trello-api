242c5c79b8cc66392feadf6ab44e02bc2325b064
Dile <duyle131103@gmail.com>
2024-05-20 23:59:47 +0700

    Commit

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,12 +1,9 @@
-const MONGODB_URI = 'mongodb+srv://leanhduy:VCMIytSum4izjxCp@cluster0.xbgkos5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-const DATABASE_NAME = 'trello-database'
-
 import { MongoClient, ServerApiVersion } from 'mongodb'
 import { env } from '~/config/environment'
 
 let trelloDatabaseInstance = null
 
-const mongoClientInstance = new MongoClient(MONGODB_URI, {
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -17,7 +14,7 @@ const mongoClientInstance = new MongoClient(MONGODB_URI, {
 export const CONNECT_DB = async () => {
   await mongoClientInstance.connect()
 
-  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
 }
 
 export const CLOSE_DB = async () => {
