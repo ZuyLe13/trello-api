e0047085b9c75218ede775fad12dc6caa012e5a9
Dile <duyle131103@gmail.com>
2024-05-22 11:56:30 +0700

    Validation with Joi

---
+++ src/validations/boardValidation.js
@@ -0,0 +1,32 @@
+import Joi from 'joi'
+import { StatusCodes } from 'http-status-codes'
+
+const createNew = async (req, res, next) => {
+  const correctCondition = Joi.object({
+    title: Joi.string().required().min(3).max(50).trim().strict().message({
+      'any.required': 'Title is required',
+      'string.empty': 'Title is not allowed to be empty',
+      'string.min': 'Title must be at least 3 chars',
+      'string.max': 'Title must be less than or equal to 50 chars',
+      'string.trim': 'Title must not have leading or trailing whitespace'
+    }),
+    description: Joi.string().required().min(3).max(256).trim().strict()
+  })
+
+  try {
+    // Chỉ định abortEarly: false để trường hợp có nhiều lỗi validation thì trả về tất cả lỗi
+    await correctCondition.validateAsync(req.body, { abortEarly: false })
+    // next()
+    res.status(StatusCodes.CREATED).json({ message: 'POST from validation: API create new board' })
+
+  } catch (error) {
+    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+      errors: new Error(error).message
+    })
+  }
+
+}
+
+export const boardValidation = {
+  createNew
+}
