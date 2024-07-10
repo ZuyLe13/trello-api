637609fd339450cb318e8b6499ac8ee62f2ea00e
Dile <duyle131103@gmail.com>
2024-07-10 16:38:44 +0700

    Udapte config/cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173'
+  // Không cần localhost nữa vì ở file config/cors đã luôn cho phép môi trường dev (env.BUILD_MODE === 'dev')
+  // 'http://localhost:5173'
 ]
 
 export const BOARD_TYPES = {
