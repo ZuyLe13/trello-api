f681abbd299de677bcd0ba04215b39ec33c72d61
Dile <duyle131103@gmail.com>
2024-07-10 17:44:17 +0700

    Update domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 export const WHITELIST_DOMAINS = [
   // Không cần localhost nữa vì ở file config/cors đã luôn cho phép môi trường dev (env.BUILD_MODE === 'dev')
   // 'http://localhost:5173'
+  'https://trello-clone-eight-wheat.vercel.app/'
 ]
 
 export const BOARD_TYPES = {
